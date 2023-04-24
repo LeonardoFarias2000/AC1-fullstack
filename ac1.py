c886e70f808172efdd88f94a18806d7d1f18318f
Leonardo Farias <90436646+LeonardoFarias2000@users.noreply.github.com>
2023-04-23 21:14:47 -0300

    commit 2 ajustes notas
    ajuste notas

--- ac1.py
+++ ac1.py
@@ -1,14 +1,34 @@
-from flask import  Flask  , request
+from flask import Flask, request, jsonify , render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-   nota1 = request.args.get('primeira')
-   nota2 = request.args.get('segunda')
-   #http://127.0.0.1:5000/?primeira=5&segunda=10
-   
-   return  ' a média é : ' + (int(nota1)+ int(nota2))/ 2
+@app.route('/',) 
+def main():   
+  return 'Bem-Vindo';
 
-if __name__== '__main__':
-    app.run(debug=True)
+@app.route('/nota',) 
+def note():   
+    situacao = None
+    media = None
+
+    nota1 = request.args.get('nota1')
+    nota2 = request.args.get('nota2')
+
+    if nota1 and nota2:
+      nota1 = float(nota1)
+      nota2 = float(nota2)
+
+    media = (nota1 + nota2) / 2
+    if media >= 7:
+      situacao = 'Aprovado'
+    elif media >= 4:
+      situacao = 'DP'
+    else:
+      situacao = 'Reprovado'
+
+    return render_template('index.html', media=media,
+                                         resultado=situacao)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
